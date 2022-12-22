01fc6fb1213536ddbb04a1f26c355fd8b074dda8
Henry F. Dotson <henryfdotson@gmail.com>
2022-12-21 22:05:05 -0500

    Added About and Contact pages. Updated styles, added images.

---
+++ about.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Henry F. Dotson</title>
+  <link href="style.css" rel="stylesheet" />
+  <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap" rel="stylesheet">
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap');
+  </style>
+</head>
+<body>
+
+<div class="backbutton"><a href="/">Back</a></div>
+<div id="about">
+    <div id="aboutImage">
+      <img src="bioimage.jpg" alt="" />
+    </div>
+    <div id="aboutDescription">
+      <p>Henry Dotson is a Philadelphia-based artist and IT specialist working at Girard College. At Girard, he is responsible for maintaining the school's network systems and ensuring that all students and staff have access to reliable technology. In addition to this, Henry has completed a four-year project called "Art Every Day" and is currently developing a video game titled "Ghosted". In the future, he hopes to combine his love of art and computer systems in whatever form that may take.</p>
+    </div>
+
+</div>
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Henry F. Dotson</title>
+  <link href="style.css" rel="stylesheet" />
+  <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap" rel="stylesheet">
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap');
+  </style>
+</head>
+<body>
+<div id="contact" >
+<a href="/">Back</a>
+
+<ul class="contactList">
+    <li class="contact-item">Email:</li>
+    <ul>
+      <li class="contact-item">Personal: henryfdotson@gmail.com</li>
+      <li class="contact-item">Work: hdotson@girardcollege.edu</li>
+    </ul>
+    
+    <li class="contact-item">LinkedIn:</li>
+    <ul>
+        <li><a href="https://www.linkedin.com/in/henryfdotson/" target="_blank">https://www.linkedin.com/in/henryfdotson/</a></li>
+    </ul>
+    <li class="contact-item">Instagram:</li>
+    <ul>
+        <li><a href="https://www.instagram.com/henryfdotson" target="_blank">https://www.instagram.com/henryfdotson</a></li>
+    </ul>
+    <li class="contact-item">YouTube:</li>
+    <ul>
+        <li><a href="https://www.youtube.com/@henryfdotson" target="_blank">https://www.youtube.com/@henryfdotson</a></li>
+    </ul>
+    <li class="contact-item">Twitter:</li>
+    <ul>
+        <li><a href="https://twitter.com/henryfdotson" target="_blank">https://twitter.com/henryfdotson</a></li>
+    </ul>
+</ul>
+</div>
+</body>
+</html>
